c8470ffcec8cc05157bd87c3ec5749d67608fa85
Jagadeeshjaggu9491 <jagadeeshjaggu9491@gmail.com>
2023-06-02 10:55:32 +0530

    header completed with responsive

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+// for navbar
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
+    search.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+// for search 
+let search = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+// for cart
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+    cartItem.classList.remove('active');
 }
